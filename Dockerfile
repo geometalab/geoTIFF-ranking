d1007150bfd8610f5a34ea25926df2015bf4ef90
underwhelmingToaster <65400197+underwhelmingToaster@users.noreply.github.com>
2021-12-27 15:22:48 +0100

    Using docker for more consistant

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM ubuntu
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update
+RUN apt -y upgrade
+RUN apt install -y python3-pip
+RUN pip install numpy
+RUN apt-get install software-properties-common -y
+RUN add-apt-repository ppa:ubuntugis/ppa
+RUN apt-get update
+RUN apt-get install gdal-bin -y
+RUN apt-get install libgdal-dev -y
+RUN export CPLUS_INCLUDE_PATH=/usr/include/gdal
+RUN export C_INCLUDE_PATH=/usr/include/gdal
+RUN pip install GDAL
+
+WORKDIR /usr/src/app
+COPY . .
--- main.py
+++ main.py
@@ -1,7 +1,54 @@
-from osgeo import gdal
-from osgeo import ogr
-from osgeo import osr
-from osgeo import gdal_array
-from osgeo import gdalconst
+import sys
+import textwrap
 
-path = "/out.tif"
+
+from numbers import Real
+from osgeo import gdalconst, osr, gdal
+
+
+def val_at_coord(filename: str,
+                 longitude: Real, latitude: Real, coordtype_georef: bool,
+                 print_xy: bool, print_values: bool):
+    ds = gdal.Open(filename, gdal.GA_ReadOnly)
+    if ds is None:
+        raise Exception('Cannot open %s' % filename)
+
+    # Build Spatial Reference object based on coordinate system, fetched from the opened dataset
+    if coordtype_georef:
+        X = longitude
+        Y = latitude
+    else:
+        srs = osr.SpatialReference()
+        srs.ImportFromWkt(ds.GetProjection())
+
+        srsLatLong = srs.CloneGeogCS()
+        # Convert from (longitude,latitude) to projected coordinates
+        ct = osr.CoordinateTransformation(srsLatLong, srs)
+        (X, Y, height) = ct.TransformPoint(longitude, latitude)
+
+    # Read geotransform matrix and calculate corresponding pixel coordinates
+    geotransform = ds.GetGeoTransform()
+    inv_geotransform = gdal.InvGeoTransform(geotransform)
+    x = int(inv_geotransform[0] + inv_geotransform[1] * X + inv_geotransform[2] * Y)
+    y = int(inv_geotransform[3] + inv_geotransform[4] * X + inv_geotransform[5] * Y)
+
+    if print_xy:
+        print('x=%d, y=%d' % (x, y))
+
+    if x < 0 or x >= ds.RasterXSize or y < 0 or y >= ds.RasterYSize:
+        raise Exception('Passed coordinates are not in dataset extent')
+
+    res = ds.ReadAsArray(x, y, 1, 1)
+    if print_values:
+        if len(res.shape) == 2:
+            print(res[0][0])
+        else:
+            for val in res:
+                print(val[0][0])
+
+    return res
+
+
+if __name__ == '__main__':
+    path = "out.tif"
+    val_at_coord(path, 1, 1, True, True, True)
