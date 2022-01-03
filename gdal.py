7c3fcce20282c37b61153d23e3a7f06901e06b06
underwhelmingToaster <65400197+underwhelmingToaster@users.noreply.github.com>
2022-01-03 17:41:39 +0100

    reformatting and cleanup

--- gdal.py
+++ gdal.py
@@ -7,14 +7,16 @@ tifPath = "out.tif"
 jsonPath = "export.geojson"
 outputPath = "output.geojson"
 
+
 def val_at_coord(coordinates):
-    result = subprocess.run(['gdallocationinfo', tifPath, '-xml', '-wgs84', str(coordinates[0]), str(coordinates[1])], capture_output=True, text=True)
+    result = subprocess.run(['gdallocationinfo', tifPath, '-xml', '-wgs84', str(coordinates[0]), str(coordinates[1])],
+                            capture_output=True, text=True)
+    print(str(coordinates[0]))
     if result.stderr != "":
         raise Exception("Exeption while excecuting gdallocationinfo: " + result.stderr)
     xml = result.stdout
-    result = xml[xml.find('<Value>') + 7 : xml.find('</Value>')]
+    result = xml[xml.find('<Value>') + 7: xml.find('</Value>')]
     return result
-
 
 
 if __name__ == '__main__':
@@ -24,28 +26,27 @@ if __name__ == '__main__':
     # Open the copied geoJson file
     with open(outputPath, "r+", encoding='utf-8') as f:
         data = json.load(f)
-        progress = 1 # For progress indicator
+        progress = 1  # For progress indicator
         for feature in data['features']:
-            print("Getting tilecounts: %s of %s"%(progress, str(len(data["features"]))))
+            print("Getting tilecounts: %s of %s" % (progress, str(len(data["features"]))))
             value = val_at_coord(feature['geometry']['coordinates'])
             feature.update({"tilecount": value})
             progress += 1
-            
+
         # Sort features by tilecount, highest first
         print("Sorting dataset...")
-        data['features'] = sorted(data['features'], key=lambda x : x['tilecount'])
+        data['features'] = sorted(data['features'], key=lambda x: x['tilecount'])
         print("Dataset sorted.")
 
         progress = 1
         for feature in data['features']:
-            print("Ranking tilecounts: %s of %s"%(progress, str(len(data["features"]))))
+            print("Ranking tilecounts: %s of %s" % (progress, str(len(data["features"]))))
             feature.update({"rank": progress})
             progress += 1
-            
+
         # Clear old data and save data to file
         f.seek(0)
         f.truncate
         json.dump(data, f, indent=4)
 
     print("Done. \n")
-
