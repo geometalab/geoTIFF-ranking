05bc7f187bcf022ea6637421957bc55b759277db
underwhelmingToaster <65400197+underwhelmingToaster@users.noreply.github.com>
2022-01-04 09:26:41 +0100

    file finding and other fixes

--- gdal.py
+++ gdal.py
@@ -1,17 +1,18 @@
+import os
 import sys
 import subprocess
 import json
 import shutil
+from os.path import exists
 
-tifPath = "out.tif"
-jsonPath = "export.geojson"
-outputPath = "output.geojson"
+tif_path = None
+json_path = None
+output_path = "output.geojson"
 
 
 def val_at_coord(coordinates):
-    result = subprocess.run(['gdallocationinfo', tifPath, '-xml', '-wgs84', str(coordinates[0]), str(coordinates[1])],
+    result = subprocess.run(['gdallocationinfo', tif_path, '-xml', '-wgs84', str(coordinates[0]), str(coordinates[1])],
                             capture_output=True, text=True)
-    print(str(coordinates[0]))
     if result.stderr != "":
         raise Exception("Exeption while excecuting gdallocationinfo: " + result.stderr)
     xml = result.stdout
@@ -19,34 +20,75 @@ def val_at_coord(coordinates):
     return result
 
 
-if __name__ == '__main__':
+def find_file(extensions):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    num_of_occurrences = 0
+
+    found_file = 0
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            for extension in extensions:
+                if file.endswith(extension):
+                    if file != output_path:
+                        num_of_occurrences += 1
+                        found_file = file
+                    else:
+                        print("Can not use %s as this is an output file." % output_path)
+
+    if num_of_occurrences == 0:
+        print('Could not find any %s files. Please place one in the same directory as this script.' % extensions)
+        exit()
+
+    if num_of_occurrences == 1:
+        print('Using %s as %s file.' % (found_file, extensions))
+        return found_file
+
+    if num_of_occurrences > 1:
+        found_file = input('Multiple %s files found. Please enter the %s file which you want to use: \n' % (extensions, extensions))
+        if exists(found_file):
+            return found_file
+        else:
+            print("File %s does not exist" % found_file)
+            exit()
+
+
+def main():
+    global tif_path
+    global json_path
+    tif_path = find_file(['.tif', 'tiff'])
+    json_path = find_file(['.geojson'])
+
     # Copy the geojson as to not edit the original data
-    shutil.copy(jsonPath, outputPath)
+    shutil.copy(json_path, output_path)
 
     # Open the copied geoJson file
-    with open(outputPath, "r+", encoding='utf-8') as f:
+    with open(output_path, "r+", encoding='utf-8') as f:
         data = json.load(f)
         progress = 1  # For progress indicator
         for feature in data['features']:
             print("Getting tilecounts: %s of %s" % (progress, str(len(data["features"]))))
             value = val_at_coord(feature['geometry']['coordinates'])
-            feature.update({"tilecount": value})
+            feature.update({"tile count": value})
             progress += 1
 
         # Sort features by tilecount, highest first
         print("Sorting dataset...")
-        data['features'] = sorted(data['features'], key=lambda x: x['tilecount'])
+        data['features'] = sorted(data['features'], key=lambda x: x['tile count'])
         print("Dataset sorted.")
 
         progress = 1
         for feature in data['features']:
-            print("Ranking tilecounts: %s of %s" % (progress, str(len(data["features"]))))
+            print("Ranking tile counts: %s of %s" % (progress, str(len(data["features"]))))
             feature.update({"rank": progress})
             progress += 1
 
         # Clear old data and save data to file
         f.seek(0)
-        f.truncate
+        f.truncate()
         json.dump(data, f, indent=4)
 
     print("Done. \n")
+
+
+if __name__ == '__main__':
+    main()
