9e22fe049580745c7bce3c1833d5e67aec58d2d4
underwhelmingToaster <65400197+underwhelmingToaster@users.noreply.github.com>
2022-01-17 17:41:13 +0100

    react re-push 2

--- distribution-web-viewer/src/App.tsx
+++ distribution-web-viewer/src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import "./App.css"
+import "./components/uploadButton";
+import PreviewWindow from "./components/previewWindow";
+import UploadButton from "./components/uploadButton";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component<any, any>{
+
+  state = {
+    fileContent: "",
+  }
+
+  handleCallback = (childData: any) =>{
+    this.setState({fileContent: childData})
+  }
+
+  render() {
+    return (
+        <div className={"App"}>
+            <UploadButton parentCallback = {this.handleCallback} />
+            <PreviewWindow content={this.state.fileContent} />
+        </div>
+
+    );
+  }
 }
 
 export default App;
