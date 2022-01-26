e96d44d2ec44e52cad3af74be44cd9acbd86f4b1
underwhelmingToaster <65400197+underwhelmingToaster@users.noreply.github.com>
2022-01-26 12:18:34 +0100

    fixed app not working when adding two subsequent graphs with the same import mode

--- distribution-web-viewer/src/App.tsx
+++ distribution-web-viewer/src/App.tsx
@@ -3,26 +3,23 @@ import "./App.css"
 import UploadButton from "./components/uploadButton";
 import Dropdown from "./components/dropdown";
 import Chart from "./components/Chart";
-import update from 'react-addons-update';
 
 
 class App extends React.Component<any, any>{
 
     handleFileCallback = (childData: any, childName: any) => {
+        let importMode: any = this.state.currentImportMode
         this.setState({
             fileNames: this.state.fileNames.concat(childName),
-            fileContents: this.state.fileContents.concat(childData)
+            fileContents: this.state.fileContents.concat(childData),
+            importModes: this.state.importModes.concat(importMode),
         })
     }
 
     handleImportCallback = (importMode: any) => {
-        this.setState(update(this.state, {
-            importMode: {
-                [this.state.fileContents.length]: {
-                    $set: importMode
-                }
-            }
-        }))
+        this.setState({
+            currentImportMode: importMode
+        })
     }
 
 
@@ -30,31 +27,32 @@ class App extends React.Component<any, any>{
         fileNames: [],
         fileContents: [],
         selectedKey: 0,
-        importMode: ["Array"],
+        importModes: [],
+        currentImportMode: "Array"
   }
 
   render() {
     if(this.state.fileContents !== null) {
         if(
             this.state.fileContents.length !== this.state.fileNames.length || (
-                this.state.fileContents.length !== this.state.importMode.length &&
-                this.state.fileContents.length !== this.state.importMode.length -1
+                this.state.fileContents.length !== this.state.importModes.length &&
+                this.state.fileContents.length !== this.state.importModes.length -1
             )
         ) {
             console.error("Not all state items are the same length: ")
             console.error(this.state.fileContents)
             console.error(this.state.fileNames)
-            console.error(this.state.importMode)
+            console.error(this.state.importModes)
         }
     }
 
 
     let graph: any;
-    if(this.state.fileContents.length === 0) {
+    if(this.state.fileContents?.length === 0) {
         graph = <p>No file selected</p>
     } else {
         graph = <div className={"Graph"}>
-            <Chart fileContent={this.state.fileContents} titles={this.state.fileNames} importMode={this.state.importMode}/>
+            <Chart fileContent={this.state.fileContents} titles={this.state.fileNames} importMode={this.state.importModes}/>
         </div>
     }
 
